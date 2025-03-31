ec642dde049bf5cce8a98950335d7d60719acdbb
rimex3 <faresahmed51857@gmail.com>
2025-03-31 18:12:45 +0200

    Remove EdgeStore integration and related components from the application

--- src/lib/edgestore.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { EdgeStoreRouter } from '@/app/edgestore/[...edgestore]/route';
-import { createEdgeStoreProvider } from '@edgestore/react';
-
-const { EdgeStoreProvider, useEdgeStore } =
-    createEdgeStoreProvider<EdgeStoreRouter>();
-
-export { EdgeStoreProvider, useEdgeStore };
