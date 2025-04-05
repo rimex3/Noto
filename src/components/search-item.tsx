7d9663bd0d7e4a9c8738b3aa91abac058952f13f
rimex3 <faresahmed51857@gmail.com>
2025-04-05 21:12:08 +0200

    feat: wrap SearchItem component with NotoTooltip for enhanced user interaction

--- src/components/search-item.tsx
+++ src/components/search-item.tsx
@@ -4,16 +4,19 @@
 import { icons } from "@/constants/icons"
 import SidebarItem from "./sidebar-item"
 import { useSearch } from "@/hooks/use-search"
+import NotoTooltip from "./noto-tooltip"
 
 export default function SearchItem() {
     const search = useSearch()
     return (
-        <div onClick={search.onOpen}>
-            <SidebarItem>
-                <div className="flex items-center space-x-1 text-[#5E5C57] font-medium text-[13px]">
-                    <span>{icons.search}</span> <span>Search</span>
-                </div>
-            </SidebarItem>
-        </div>
+        <NotoTooltip content="Ctrl + K">
+            <div onClick={search.onOpen}>
+                <SidebarItem>
+                    <div className="flex items-center space-x-1 text-[#5E5C57] font-medium text-[13px]">
+                        <span>{icons.search}</span> <span>Search</span>
+                    </div>
+                </SidebarItem>
+            </div>
+        </NotoTooltip>
     )
 }
